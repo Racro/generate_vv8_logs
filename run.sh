0d39deb55e6b63608c28978370210bca5a5f8a7b
Racro <ritik.r@nyu.edu>
2024-08-21 20:29:40 +0000

    add directory

--- run.sh
+++ run.sh
@@ -2,18 +2,18 @@
 
 # URL='https://www.geeksforgeeks.org/deletion-in-linked-list/';
 # URL='https://www.cricbuzz.com'
-URL='websites.txt'
+URL='websites_1000.txt'
 
 # pushd ./verify > /dev/null
 # for i in $(seq 1 5);
 # do
     # node check_selector.js --headless=false --extn=control --url=$URL 
-# python3 generate_parallel_logs.py --headless 'true' --extn $1 --url $2 
+python3 generate_parallel_logs.py --headless 'true' --extn $1 --url $2 --directory $3
 # sleep 2
 # python3 clean.py --extn control --site $URL
 # done
 
-# sleep 5
+sleep 5
 
 # # for i in $(seq 1 5);
 # # do
@@ -26,12 +26,12 @@ URL='websites.txt'
 
 # sleep 5
 
-python3 process_v8_logs.py --extn $1 --url $URL
+python3 process_v8_logs.py --extn $1 --url $URL --directory $3
 # # python3 process_v8_logs.py --extn ublock --url $URL
 
 sleep 5
 
-python3 intersection.py --extn $1 --url $URL
+python3 intersection.py --extn $1 --url $URL --directory $3
 # python3 intersection.py --extn ublock --url $URL
 
 # sleep 5
